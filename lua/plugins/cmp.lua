dc7fcaf2b836cd1f9a17f4df3da6d223f57efd46
TimovVeen <timovanveen@hotmail.nl>
2025-02-06 04:02:06 +0100

    blink cmp

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,29 @@
+return {
+  'saghen/blink.cmp',
+  -- optional: provides snippets for the snippet source
+  dependencies = 'rafamadriz/friendly-snippets',
+
+  -- use a release tag to download pre-built binaries
+  version = '*',
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    -- 'default' for mappings similar to built-in completion
+    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+    -- See the full "keymap" documentation for information on defining your own keymap.
+    keymap = { preset = 'default' },
+
+    appearance = {
+      nerd_font_variant = 'mono',
+    },
+
+    -- Default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, due to `opts_extend`
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+    },
+  },
+  opts_extend = { 'sources.default' },
+}
